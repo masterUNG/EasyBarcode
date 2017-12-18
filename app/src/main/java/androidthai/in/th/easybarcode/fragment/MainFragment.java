8fb64a6a18939a3cdfcec8cfd9e9810ebbb877c9
MasterUNG <phrombutr@gmail.com>
2017-12-18 14:26:05 +0700

    Scan BarCode Success

--- app/src/main/java/androidthai/in/th/easybarcode/fragment/MainFragment.java
+++ app/src/main/java/androidthai/in/th/easybarcode/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package androidthai.in.th.easybarcode.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,8 +9,10 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidthai.in.th.easybarcode.R;
 
@@ -31,8 +34,58 @@ public class MainFragment extends Fragment{
 //        For Special
         forSpecial();
 
+//        Scan Controller
+        scanController();
 
     }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            String resultString = data.getStringExtra("SCAN_RESULT");
+            textView.setText(resultString);
+
+        } else {
+            Toast.makeText(getActivity(), "Un Success",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void scanController() {
+
+        Button button = getView().findViewById(R.id.btnScan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String myString = "com.google.zxing.client.android.SCAN";
+                String modeString = "BAR_CODE_MODE";
+
+                try {
+
+                    Intent intent = new Intent(myString);
+                    intent.putExtra("SCAN_MODE", modeString);
+                    startActivityForResult(intent, 1);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Please Install QR Barcode",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+    }
 
     private void initialTextView() {
         textView = getView().findViewById(R.id.txtDisplay);
