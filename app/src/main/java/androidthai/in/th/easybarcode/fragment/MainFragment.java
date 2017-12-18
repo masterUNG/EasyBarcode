7e250e11bf6f213fe8a5b90750f00e12eb56fb05
MasterUNG <phrombutr@gmail.com>
2017-12-18 13:42:28 +0700

    For Special OK

--- app/src/main/java/androidthai/in/th/easybarcode/fragment/MainFragment.java
+++ app/src/main/java/androidthai/in/th/easybarcode/fragment/MainFragment.java
@@ -3,9 +3,13 @@ package androidthai.in.th.easybarcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidthai.in.th.easybarcode.R;
 
@@ -14,6 +18,49 @@ import androidthai.in.th.easybarcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+    private TextView textView;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial TextView
+        initialTextView();
+
+//        For Special
+        forSpecial();
+
+
+    }   // Main Method
+
+    private void initialTextView() {
+        textView = getView().findViewById(R.id.txtDisplay);
+    }
+
+    private void forSpecial() {
+
+        EditText editText = getView().findViewById(R.id.edtSpecial);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                textView.setText(s);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
 
     @Nullable
     @Override
